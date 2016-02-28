77c19618b534aa4ae52e7991f554b3b1841896c4
Gonzalo Fernández <gonzalo.fernandez@mo2o.com>
2016-02-28 11:55:30 +0100

    Added the interface of the view to be implemented on the activity

---
+++ presentation/src/main/java/szgrc/flickrsearching/HomeSearchView.java
@@ -0,0 +1,15 @@
+package szgrc.flickrsearching;
+
+/**
+ * Created by gonzalofernandez on 28/2/16.
+ */
+public interface HomeSearchView {
+
+    void showToolbarSearchable();
+
+    void hideToolbarSearchable();
+
+    void showFAB();
+
+    void hideFAB();
+}
